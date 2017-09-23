4d5180f02619f4904ed65a77ffcfde794417d3da
jaroslaw <jarek.skrzypczak@o2.pl>
2017-09-23 12:12:33 +0200

    Add pictures. Change layout. Fight with positioning.

--- app/src/main/java/com/example/jaroslaw/contour/MainActivity.java
+++ app/src/main/java/com/example/jaroslaw/contour/MainActivity.java
@@ -4,11 +4,10 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.OrientationEventListener;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
@@ -16,10 +15,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     String TAG = "*********";
 
     private TextView axisX, axisY, axisZ;
+    private ImageView bubble, areaXY;
     private SensorManager sensorManager;
     private Sensor rotationSensor;
     private float[] rotationVector;
-    private float maxx = 0, maxy = 0, maxz = 0;
+    private double trans = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         axisX = (TextView) findViewById(R.id.axis_X);
         axisY = (TextView) findViewById(R.id.axis_Y);
         axisZ = (TextView) findViewById(R.id.axis_Z);
+        bubble = (ImageView) findViewById(R.id.imageBubble);
+        areaXY = (ImageView) findViewById(R.id.imageArea);
 
         sensorManager = (SensorManager)getApplicationContext().getSystemService(SENSOR_SERVICE);
         rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -72,40 +74,46 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             rotationVector = sensorEvent.values;
             String xstring, ystring, zstring;
 
-            maxx = Math.max(maxx, rotationVector[0]);
-            maxy = Math.max(maxy, rotationVector[1]);
-            maxz = Math.max(maxz, rotationVector[2]);
-
             if(rotationVector[0]>0){
                 xstring = "left";
             }else{
                 xstring = "right";
             }
-            xstring += " " + maxx + " " + rotationVector[0];
             if(rotationVector[1]>0){
                 ystring = "down";
             }else{
                 ystring = "up";
             }
-            ystring += " " + maxy + " "  + rotationVector[1];
             if(rotationVector[2]>0){
                 zstring = "forward";
             }else{
                 zstring = "back";
             }
-            zstring += " " + maxz + " "  + rotationVector[2];
-            axisX.setText(xstring);
-            axisY.setText(ystring);
-            axisZ.setText(zstring);
+            axisX.setText(xstring+rotationVector[0]);
+            axisY.setText(ystring+rotationVector[1]);
+            axisZ.setText(zstring+rotationVector[2]);
+
+            bubble.setPadding(areaXY.getWidth()/2+(int)trans, areaXY.getHeight()/2,0,0);
+            int[] padings = {bubble.getPaddingLeft(), bubble.getPaddingTop(), bubble.getPaddingRight(), bubble.getPaddingBottom()};
+      //      trans += 0.5;
+
             double x = (double) Math.abs(rotationVector[0]);
             double y = (double) Math.abs(rotationVector[1]);
             double z = (double) Math.abs(rotationVector[2]);
             double angleAlpha = Math.acos((x*x+y*y)/(Math.sqrt(x*x+y*y+z*z)*Math.sqrt(x*x+y*y)));//field xy to axis z
             double angleBeta = Math.acos((y*y+z*z)/(Math.sqrt(x*x+y*y+z*z)*Math.sqrt(y*y+z*z)));//field yz to axis x
             double angleGamma = Math.acos((z*z+x*x)/(Math.sqrt(x*x+y*y+z*z)*Math.sqrt(z*z+x*x)));//field zx to axis y
+
+            for (int w : padings) {
+                Log.d(TAG, "Paddings = " + w);
+            }
+            Log.d(TAG, "area: "+ areaXY.getWidth()+"  "+ areaXY.getHeight());
+            Log.d(TAG, "bubble: "+ bubble.getWidth()+"  "+ bubble.getHeight());
+            Log.d(TAG, "Transition = "+ trans);
             Log.d(TAG, "onSensorChanged: angleAlpha "+ Math.toDegrees(angleAlpha));
             Log.d(TAG, "onSensorChanged: angleBeta "+ Math.toDegrees(angleBeta));
             Log.d(TAG, "onSensorChanged: angleGamma "+ Math.toDegrees(angleGamma));
+
         }
 
     }
