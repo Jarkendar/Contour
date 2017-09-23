510fe0cffdb390ec7f8094a9a4ef6db4eca5407d
jaroslaw <jarek.skrzypczak@o2.pl>
2017-09-23 13:19:26 +0200

    Add method: makeLinearFunction, calculateAParamater, getMaxTransistion. Create function and calculate maximum change posiotnion.

--- app/src/main/java/com/example/jaroslaw/contour/MainActivity.java
+++ app/src/main/java/com/example/jaroslaw/contour/MainActivity.java
@@ -93,19 +93,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             axisY.setText(ystring+rotationVector[1]);
             axisZ.setText(zstring+rotationVector[2]);
 
-            int areaXYWidth = areaXY.getWidth();
-            int areaXYHeight = areaXY.getHeight();
-            bubble.setPivotX(areaXYWidth/2);
-            bubble.setPivotY(areaXYHeight/2);
-            bubble.setPadding(centerBubbleXY(areaXYWidth), centerBubbleXY(areaXYHeight),0,0);
-        //    bubble.setRotation(trans);
-            int[] padings = {bubble.getPaddingLeft(), bubble.getPaddingTop(), bubble.getPaddingRight(), bubble.getPaddingBottom()};
-            trans += 0.5;
-            //max left 188
-            //max top
-            //max right
-            //max bottom
-
             double x = (double) Math.abs(rotationVector[0]);
             double y = (double) Math.abs(rotationVector[1]);
             double z = (double) Math.abs(rotationVector[2]);
@@ -113,12 +100,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             double angleBeta = Math.acos((y*y+z*z)/(Math.sqrt(x*x+y*y+z*z)*Math.sqrt(y*y+z*z)));//field yz to axis x
             double angleGamma = Math.acos((z*z+x*x)/(Math.sqrt(x*x+y*y+z*z)*Math.sqrt(z*z+x*x)));//field zx to axis y
 
+            int areaXYWidth = areaXY.getWidth();
+            int areaXYHeight = areaXY.getHeight();
+            bubble.setPivotX(areaXYWidth/2);
+            bubble.setPivotY(areaXYHeight/2);
+            bubble.setPadding(centerBubbleXY(areaXYWidth), centerBubbleXY(areaXYHeight),0,0);
+            bubble.setRotation(trans);
+            int[] padings = {bubble.getPaddingLeft(), bubble.getPaddingTop(), bubble.getPaddingRight(), bubble.getPaddingBottom()};
+            trans++;
+            //max left 188
+            //max top
+            //max right
+            //max bottom
+
             for (int w : padings) {
                 Log.d(TAG, "Paddings = " + w);
             }
 
             Log.d(TAG, "area: "+ areaXY.getWidth()+"  "+ areaXY.getHeight());
             Log.d(TAG, "bubble: "+bubble.getDrawable().getIntrinsicWidth()+"  "+bubble.getDrawable().getIntrinsicHeight());
+            Log.d(TAG, "bubble: "+bubble.getWidth()+"  "+bubble.getHeight());
             Log.d(TAG, "Transition = "+ trans);
             Log.d(TAG, "Rotation = "+ bubble.getRotation());
             Log.d(TAG, "Pivot Point =" +bubble.getPivotX() + " "+ bubble.getPivotY());
@@ -127,6 +128,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Log.d(TAG, "onSensorChanged: angleGamma "+ Math.toDegrees(angleGamma));
 
         }
+    }
+
+    private int getMaxTransition(int areaWidth, int bubbleWidth){
+        return areaWidth/2-2*bubbleWidth/3;
+    }
+
+    private float makeLinearFunction(int maxTransition){
+        float minAngle = 0.0f, maxAngle = 15.0f;
+        return calculateAParameter(maxTransition, minAngle,maxAngle); //b always = 0,
+    }
+
+    private float calculateAParameter(int maxTransition, float minAngle, float maxAngle){
+        return maxTransition/(maxAngle-minAngle);
     }
 
     private int centerBubbleXY(int lenght){
