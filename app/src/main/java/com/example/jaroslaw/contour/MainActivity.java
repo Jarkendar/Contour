3b731d705603d746a18d21f96ce0ea64ec044cd5
jaroslaw <jarek.skrzypczak@o2.pl>
2017-09-24 13:55:35 +0200

    Refactoring code. Create new method. Delete usuless variable.

--- app/src/main/java/com/example/jaroslaw/contour/MainActivity.java
+++ app/src/main/java/com/example/jaroslaw/contour/MainActivity.java
@@ -18,9 +18,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private ImageView bubble, areaXY;
     private SensorManager sensorManager;
     private Sensor rotationSensor;
-    private float[] rotationVector;
-    private float function;
-    private float minAngle = 0.0f, maxAngle = 25.0f;
+    private float aParameter;
+    private float maxAngle = 25.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +33,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         sensorManager = (SensorManager) getApplicationContext().getSystemService(SENSOR_SERVICE);
         rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        for (Sensor x : sensorManager.getSensorList(Sensor.TYPE_ALL)) {
-            Log.d(TAG, "onCreate: " + x.toString());
-        }
     }
 
     @Override
@@ -59,81 +55,74 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int i) {
     }
 
-    boolean count = false;
-    long start;
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-
-            if (count) {
-                Log.d(TAG, "onSensorChanged: time " + (System.currentTimeMillis() - start));
-                start = System.currentTimeMillis();
-            } else {
-                count = true;
-                start = System.currentTimeMillis();
-            }
-            rotationVector = sensorEvent.values;
+            float[] vectorsXYZ = sensorEvent.values;
             String xstring, ystring, zstring;
 
-            if (rotationVector[0] > 0) {
+            if (vectorsXYZ[0] > 0) {
                 xstring = "left";
             } else {
                 xstring = "right";
             }
-            if (rotationVector[1] > 0) {
+            if (vectorsXYZ[1] > 0) {
                 ystring = "down";
             } else {
                 ystring = "up";
             }
-            if (rotationVector[2] > 0) {
+            if (vectorsXYZ[2] > 0) {
                 zstring = "forward";
             } else {
                 zstring = "back";
             }
-            axisX.setText(xstring + rotationVector[0]);
-            axisY.setText(ystring + rotationVector[1]);
-            axisZ.setText(zstring + rotationVector[2]);
+            axisX.setText(xstring + vectorsXYZ[0]);
+            axisY.setText(ystring + vectorsXYZ[1]);
+            axisZ.setText(zstring + vectorsXYZ[2]);
 
-            double x = (double) Math.abs(rotationVector[0]);
-            double y = (double) Math.abs(rotationVector[1]);
-            double z = (double) Math.abs(rotationVector[2]);
-            double angleAlpha = Math.toDegrees(Math.acos((x * x + y * y) / (Math.sqrt(x * x + y * y + z * z) * Math.sqrt(x * x + y * y))));//field xy to axis z
-            double angleBeta = Math.toDegrees(Math.acos((y * y + z * z) / (Math.sqrt(x * x + y * y + z * z) * Math.sqrt(y * y + z * z))));//field yz to axis x
-            double angleGamma = Math.toDegrees(Math.acos((z * z + x * x) / (Math.sqrt(x * x + y * y + z * z) * Math.sqrt(z * z + x * x))));//field zx to axis y
+            double x = vectorsXYZ[0];
+            double y = vectorsXYZ[1];
+            double z = vectorsXYZ[2];
+            double angleXYtoZ = Math.toDegrees(Math.acos((x * x + y * y) / (Math.sqrt(x * x + y * y + z * z) * Math.sqrt(x * x + y * y))));//field xy to axis z
+            double angleYZtoX = Math.toDegrees(Math.acos((y * y + z * z) / (Math.sqrt(x * x + y * y + z * z) * Math.sqrt(y * y + z * z))));//field yz to axis x
+            double angleZXtoY = Math.toDegrees(Math.acos((z * z + x * x) / (Math.sqrt(x * x + y * y + z * z) * Math.sqrt(z * z + x * x))));//field zx to axis y
 
-            double angleDelta;//angle x to y
+            double angleCtoX;//angle c to x, c is hypotenuse
             if (x != 0) {
-                angleDelta = Math.toDegrees(Math.atan(y / x));
+                angleCtoX = Math.toDegrees(Math.atan( Math.abs(y) / Math.abs(x)));
             } else {
-                angleDelta = 90;
+                angleCtoX = 90;
             }
 
-            if (angleAlpha < (90 - maxAngle)) angleAlpha = 90 - maxAngle;
-            if (angleBeta < (90 - maxAngle)) angleBeta = 90 - maxAngle;
-            if (angleGamma < (90 - maxAngle)) angleGamma = 90 - maxAngle;
+            if (angleIsAboveLimit(angleXYtoZ)) angleXYtoZ = 90 - maxAngle;
+            if (angleIsAboveLimit(angleYZtoX)) angleYZtoX = 90 - maxAngle;
+            if (angleIsAboveLimit(angleZXtoY)) angleZXtoY = 90 - maxAngle;
 
-            double angleRotation = 0;
+            double rotationOnXY = 0;
 
-            if (rotationVector[0] >= 0 && rotationVector[1] >= 0) angleRotation = (90 - angleDelta) + 270;
-            if (rotationVector[0] >= 0 && rotationVector[1] <= 0) angleRotation = angleDelta;
-            if (rotationVector[0] <= 0 && rotationVector[1] >= 0) angleRotation = angleDelta + 180;
-            if (rotationVector[0] <= 0 && rotationVector[1] <= 0)
-                angleRotation = (90 - angleDelta) + 90;
+            if (vectorsXYZ[0] >= 0 && vectorsXYZ[1] >= 0) rotationOnXY = (90-angleCtoX) + 270;//quarter 1
+            if (vectorsXYZ[0] >= 0 && vectorsXYZ[1] <= 0) rotationOnXY = angleCtoX;//quarter 4
+            if (vectorsXYZ[0] <= 0 && vectorsXYZ[1] >= 0) rotationOnXY = angleCtoX + 180;//quarter 2
+            if (vectorsXYZ[0] <= 0 && vectorsXYZ[1] <= 0) rotationOnXY = (90-angleCtoX) + 90;//quarter 3
 
             int areaXYWidth = areaXY.getWidth();
             int areaXYHeight = areaXY.getHeight();
             setStartParameters(areaXYWidth, areaXYHeight);
 
-            bubble.setPadding(centerBubbleXY(areaXYWidth) + (int) (function * (90 - angleAlpha)), centerBubbleXY(areaXYHeight), 0, 0);
-            bubble.setRotation((float) angleRotation);
+            bubble.setPadding(centerBubbleXY(areaXYWidth) + (int) (aParameter * (90 - angleXYtoZ)), centerBubbleXY(areaXYHeight), 0, 0);
+            bubble.setRotation((float) rotationOnXY);
+
         }
     }
 
+    private boolean angleIsAboveLimit(double angle) {
+        return angle < (90 - maxAngle);
+    }
 
 
     private void setStartParameters(int areaXYWidth, int areaXYHeight) {
-        function = calculateAParameter(getMaxTransition(areaXY.getWidth(), bubble.getDrawable().getIntrinsicWidth()));
+        aParameter = calculateAParameter(getMaxTransition(areaXY.getWidth(), bubble.getDrawable().getIntrinsicWidth()));
         bubble.setPivotX(areaXYWidth / 2);
         bubble.setPivotY(areaXYHeight / 2);
     }
@@ -143,6 +132,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     private float calculateAParameter(int maxTransition) {
+        float minAngle = 0.0f;
         return ((float) maxTransition) / (maxAngle - minAngle);
     }
 
